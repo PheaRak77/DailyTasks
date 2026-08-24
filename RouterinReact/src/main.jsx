92a84c99a14b22b65b81cd335462062cd100360e
PheaRak77 <renpearak6666@gmail.com>
2026-08-24 17:41:53 +0700

    Configure GitHub Pages automatic deployment and HashRouter

--- RouterinReact/src/main.jsx
+++ RouterinReact/src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js"; // optional, for JS components
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>,
 );
